fea1462758f8170514c26aa29987a7e71c81b431
dhdhagai <dhruv.h.dhagai@gmail.com>
2023-09-21 21:33:02 +0530

    Updated MainActivity and CounterActivity to add a notification when you check the "Include 1" option on the main activity

--- app/src/main/java/com/counter/dhdhagai/MainActivity.kt
+++ app/src/main/java/com/counter/dhdhagai/MainActivity.kt
@@ -1,37 +1,59 @@
 package com.counter.dhdhagai
 
+import android.app.NotificationChannel
+import android.app.NotificationChannelGroup
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.annotation.RequiresApi
 
-
-class MainActivity : ComponentActivity() {
-    lateinit var button: Button
+ class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
+        createNotificationChannel()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val myButton: Button = findViewById(R.id.button)
-
+        var isEnabled: Boolean = false
         myButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View) {
                 val myIntent = Intent(this@MainActivity, CounterActivity::class.java)
-                myIntent.putExtra("Enabled", true)
+                myIntent.putExtra("Enabled", isEnabled.toString())
                 startActivity(myIntent)
             }
         })
         val radioButton: RadioButton = findViewById(R.id.button2)
-        radioButton.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View) {
-                showToast("Enabled 1")
-            }
-        })
+        radioButton.setOnClickListener {
+        showToast("Enabled 1")
+        isEnabled = true
+        }
     }
     private fun showToast(message: String) {
         Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+    public fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_desc)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val CHANNEL_ID: String = "main_notif"
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
